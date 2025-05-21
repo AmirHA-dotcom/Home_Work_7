03521a3721e910d2b5d228b7800c48138d57b37f
Amir Hossein Abnous <amirhosseinabnous@gmail.com>
2025-05-21 11:25:01 +0330

    adding patients

--- HW7_Q1/main.cpp
+++ HW7_Q1/main.cpp
@@ -3,12 +3,16 @@
 #include <regex>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 // --------------- Regexes ---------------
 
-regex add_doc_pattern(R"(^add doctor (\w+) (\w+) (\S+) (\S+)(?: (\S+))?(?: (\S+))?(?: (\S+))?(?: (\S+))?(?: (\S+))?(?: (\S+))?$)");
+regex add_doc_pattern(R"(^add doctor (\w+) (\w+) (\S+) (\S+)(?: (\S+))?(?: (\S+))?(?: (\S+))?(?: (\S+))?(?: (\S+))?(?: (\S+))?\s*$)");
+regex change_NO_of_patients_pattern(R"(^change the number of patients of doctor (\w+) to (\S+)\s*$)");
+regex change_working_days_pattern(R"(^change the working days of doctor (\w+) to (\S+)(?: (\S+))?(?: (\S+))?(?: (\S+))?(?: (\S+))?(?: (\S+))?(?: (\S+))?\s*$)");
+regex add_patient(R"(^add patient (\w+) (\w+)\s*$)");
 smatch match;
 
 // ------------- Exceptions ------------------
@@ -45,8 +49,28 @@ public:
         return "Error: number of patients can not be negative";
     }
 };
+class doc_doesnt_exist : public exception
+{
+public:
+    const char* what() const noexcept override
+    {
+        return "Error: no doctor with this name exist";
+    }
+};
+
 
 // --------------- Model ---------------
+
+class Patient
+{
+private:
+    string name;
+    string specialty;
+public:
+    Patient(string n, string spec) : name(n), specialty(spec) {}
+    string get_name() const{return name;}
+    string get_specialty() const{return specialty;}
+};
 
 class Doc
 {
@@ -55,12 +79,57 @@ private:
     string specialty;
     int number_of_patients;
     vector<string> working_days;
+    vector<pair<Patient*, string>> patients;
+    void delete_that_day(string day)
+    {
+        patients.erase(remove_if(patients.begin(), patients.end(),[&day](const pair<Patient*, string>& entry)
+        {
+            return entry.second == day;
+        }),patients.end());
+    }
 public:
     Doc(string n, string spec, int NO_of_patients, vector<string> W_D) : name(n), specialty(spec), number_of_patients(NO_of_patients), working_days(W_D) {}
     string get_name() const{return name;}
     string get_specialty() const{return specialty;}
     int get_number_of_patients() const{return number_of_patients;}
     vector<string> get_working_days() const{return working_days;}
+    void change_NO_of_patients(int NO)
+    {
+        if (NO < number_of_patients)
+        {
+            int d = number_of_patients - NO;
+            for (int i = 0; i < d; i++)
+            {
+                if (patients[number_of_patients - i].first != nullptr)
+                    patients.erase(patients.begin() + number_of_patients - i);
+            }
+        }
+        number_of_patients = NO;
+    }
+    void change_working_days(vector<string> new_days)
+    {
+        working_days.swap(new_days);
+        if (find(working_days.begin(), working_days.end(), "saturday") == working_days.end())
+        {
+            delete_that_day("saturday");
+        }
+        if (find(working_days.begin(), working_days.end(), "sunday") == working_days.end())
+        {
+            delete_that_day("sunday");
+        }
+        if (find(working_days.begin(), working_days.end(), "monday") == working_days.end())
+        {
+            delete_that_day("monday");
+        }
+        if (find(working_days.begin(), working_days.end(), "tuesday") == working_days.end())
+        {
+            delete_that_day("tuesday");
+        }
+        if (find(working_days.begin(), working_days.end(), "wednesday") == working_days.end())
+        {
+            delete_that_day("wednesday");
+        }
+    }
 };
 
 // ------------ Controller ------------
@@ -69,6 +138,7 @@ class Controller
 {
 private:
     vector<Doc*> doctors;
+    vector<Patient*> patients;
     int doc_index_finder(Doc* temp_doc)
     {
         for (int i = 0; i < doctors.size(); i++)
@@ -109,10 +179,31 @@ public:
         doctors.push_back(new Doc(name, specialty, NO_of_patients, working_days));
         cout << "doctor " << name << " added with specialty " << specialty << " with " << NO_of_patients << " patients" << endl;
     }
+    void change_number_of_patients(string doc_name, int NO)
+    {
+        int doc_index = doc_index_finder_by_name(doc_name);
+        if (doc_index == -1)
+            throw doc_doesnt_exist();
+        if (NO < 0)
+            throw not_valid_number();
+        doctors[doc_index]->change_NO_of_patients(NO);
+        cout << "number of patients changed" << endl;
+    }
+    void change_working_days(string doc_name, vector<string> working_days)
+    {
+        int doc_index = doc_index_finder_by_name(doc_name);
+        if (doc_index == -1)
+            throw doc_doesnt_exist();
+        if (!week_day_valider(working_days))
+            throw not_valid_day();
+        cout << "working days changed" << endl;
+    }
     ~Controller()
     {
         for (Doc* doc : doctors)
             delete doc;
+        for (Patient* p : patients)
+            delete p;
     }
 };
 Controller AHA;
@@ -132,6 +223,22 @@ bool input_handler(string line)
                 days.push_back(match[4 + i]);
         }
         AHA.add_doc(match[1], match[2], stoi(match[3]), days);
+        return true;
+    }
+    else if (regex_match(line, match, change_NO_of_patients_pattern))
+    {
+        AHA.change_number_of_patients(match[1], stoi(match[2]));
+        return true;
+    }
+    else if (regex_match(line, match, change_working_days_pattern))
+    {
+        vector<string> days;
+        for (int i = 0; i < 7; i++)
+        {
+            if (match[1 + i].matched)
+                days.push_back(match[4 + i]);
+        }
+        AHA.change_working_days(match[1], days);
         return true;
     }
     else
